d960f7cde4ce9e036ce69dbcf11570ca864882e4
samuel.urbanowicz <samiuelson@gmail.com>
2017-02-17 11:46:27 +0100

    Apply NonAlphaCharsTransformer

--- app/src/main/java/co/thefabulous/search/SampleAnalyzer.java
+++ app/src/main/java/co/thefabulous/search/SampleAnalyzer.java
@@ -6,13 +6,15 @@ import java.util.Collection;
 import co.thefabulous.search.search.text.analyze.Analyzer;
 import co.thefabulous.search.search.text.analyze.tokenize.WordTokenizer;
 import co.thefabulous.search.search.text.analyze.transform.LowerCaseTransformer;
+import co.thefabulous.search.search.text.analyze.transform.NonAlphaCharsTransformer;
 
 public class SampleAnalyzer extends Analyzer {
     private Analyzer tokenizer = new WordTokenizer();
     private Analyzer transformer = new LowerCaseTransformer();
+    private Analyzer nonAlphaCharsTransformer = new NonAlphaCharsTransformer();
 
     @Override
     public Collection<String> apply(Collection<String> input) {
-        return tokenizer.apply(transformer.apply(input));
+        return tokenizer.apply(transformer.apply(nonAlphaCharsTransformer.apply(input)));
     }
 }
