65a554d75f6f112f3863cadbf4e0c323c4bd778f
Bartosz Lipinski <bartoszlipinski90@gmail.com>
2017-02-23 15:34:05 +0100

    Default options

--- app/src/main/java/co/thefabulous/search/fuse/FuseEngine.java
+++ app/src/main/java/co/thefabulous/search/fuse/FuseEngine.java
@@ -1,5 +1,6 @@
 package co.thefabulous.search.fuse;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -10,6 +11,33 @@ import co.thefabulous.search.search.data.Indexable;
  * 23.02.2017
  */
 public class FuseEngine<T extends Indexable> implements Engine<T> {
+
+    private final Options DEFAULT_OPTIONS = Options.builder()
+            .id(null)
+            .caseSensitive(false)
+            .include(new ArrayList<String>())
+            .shouldSort(true)
+            .searchFunction(null) //// TODO: 23.02.2017 set BitapSearcher
+            .sortFunction(new Options.SortFunction() {
+                @Override
+                public int sort(Options.SearchResult a, Options.SearchResult b) {
+                    return a.score() - b.score();
+                }
+            })
+            .getFunction(new Options.GetFunction() {
+                @Override
+                public void get(Object obj, Object path, Object list) {
+                    //// TODO: 23.02.2017
+                }
+            })
+            .keys(new ArrayList<String>())
+            .verbose(false)
+            .tokenize(false)
+            .matchAllTokens(false)
+            .tokenSeparator("/ +/g") //// TODO: 23.02.2017 check if correct
+            .minimumCharLength(1)
+            .findAllMatches(false)
+            .build();
     @Override
     public boolean addAll(Collection<T> dataSet) {
         return false;
